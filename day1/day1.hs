95e1791770bdaf3b16671a94409ae10018fbfced
Chua Hou <human+github@chuahou.dev>
2020-12-02 04:15:07 +0800

    day1: add part 1

---
+++ day1/day1.hs
@@ -0,0 +1,26 @@
+-- SPDX-License-Identifier: MIT
+-- Copyright (c) 2020 Chua Hou
+
+{-# LANGUAGE TupleSections #-}
+
+import           Control.Applicative ((<|>))
+import           Data.List           (sort)
+
+head' :: [a] -> Maybe a
+head' []    = Nothing
+head' (x:_) = Just x
+
+target :: Int
+target = 2020
+
+solve :: [Int] -> Maybe Int
+solve = (uncurry (*) <$>) . findPair . sort
+    where findPair :: [Int] -> Maybe (Int, Int)
+          findPair []     = Nothing
+          findPair [_]    = Nothing
+          findPair (x:xs) =   ((x,) <$> (head' . filter (== target - x)) xs)
+                          <|> findPair xs
+
+main :: IO ()
+main =   readFile "input"
+     >>= print . solve . map read . lines
