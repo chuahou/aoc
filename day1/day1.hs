3d766c65d4a6fcc5e6761659b135fef2fda7c2c4
Chua Hou <human+github@chuahou.dev>
2020-12-02 04:28:32 +0800

    day1: add part 2, refactor part 1

--- day1/day1.hs
+++ day1/day1.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE TupleSections #-}
 
 import           Control.Applicative ((<|>))
+import           Data.Functor        ((<&>))
 import           Data.List           (sort)
 
 head' :: [a] -> Maybe a
@@ -13,13 +14,25 @@ head' (x:_) = Just x
 target :: Int
 target = 2020
 
-solve :: [Int] -> Maybe Int
-solve = (uncurry (*) <$>) . findPair . sort
-    where findPair :: [Int] -> Maybe (Int, Int)
-          findPair []     = Nothing
-          findPair (x:xs) =   ((x,) <$> (head' . filter (== target - x)) xs)
-                          <|> findPair xs
+solve1 :: [Int] -> Maybe Int
+solve1 = (uncurry (*) <$>) . findPair target . sort
+
+findPair :: Int -> [Int] -> Maybe (Int, Int)
+findPair _ []     = Nothing
+findPair t (x:xs) =   (x,) <$> (head' . filter (== t - x)) xs
+                  <|> findPair t xs
+
+solve2 :: [Int] -> Maybe Int
+solve2 = fmap product' . findTriplet target . sort
+    where product' (a, b, c) = product [a, b, c]
+
+findTriplet :: Int -> [Int] -> Maybe (Int, Int, Int)
+findTriplet _ []     = Nothing
+findTriplet t (x:xs) =   (\(a, b) -> (x, a, b)) <$> findPair (t - x) xs
+                     <|> findTriplet t xs
 
 main :: IO ()
-main =   readFile "input"
-     >>= print . solve . map read . lines
+main = do { input <- readFile "input" <&> map read . lines
+          ; print . solve1 $ input
+          ; print . solve2 $ input
+          }
