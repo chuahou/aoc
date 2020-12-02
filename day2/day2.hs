cf35803b9394cde332404e0c2de8c2dffb2f5290
Chua Hou <human+github@chuahou.dev>
2020-12-02 19:38:19 +0800

    day2: extract password out of @Requirement@ tuple
    - This is more idiomatic as it separates a requirement from the password
      it should be checked against

--- day2/day2.hs
+++ day2/day2.hs
@@ -5,26 +5,27 @@ import           Control.Monad (forM_)
 import           Data.Either   (fromRight)
 import           Text.Parsec
 
-type Requirement = (Int, Int, Char, String)
+type Requirement = (Int, Int, Char)
+type Password    = String
 
-solve :: (Requirement -> Bool) -> [String] -> Int
-solve p = length . filter (fromRight False . parse (p <$> lineP) "")
+solve :: (Requirement -> Password -> Bool) -> [String] -> Int
+solve p = length . filter (fromRight False . parse (uncurry p <$> lineP) "")
 
-lineP :: Parsec String () Requirement
+lineP :: Parsec String () (Requirement, Password)
 lineP = do { low  <- read <$> many1 digit <* char '-'
            ; high <- read <$> many1 digit <* space
            ; c    <- anyChar              <* string ": "
            ; cs   <- many anyChar         <* eof
-           ; return (low, high, c, cs)
+           ; return ((low, high, c), cs)
            }
 
-check1 :: Requirement -> Bool
-check1 (low, high, c, cs) =
+check1 :: Requirement -> Password -> Bool
+check1 (low, high, c) cs =
     let cCount = length . filter (== c) $ cs
      in cCount <= high && cCount >= low
 
-check2 :: Requirement -> Bool
-check2 (x, y, c, cs) =
+check2 :: Requirement -> Password -> Bool
+check2 (x, y, c) cs =
     max x y <= length cs && (cs !! (x - 1) == c) /= (cs !! (y - 1) == c)
 
 main :: IO ()
