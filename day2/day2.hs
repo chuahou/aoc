27fe75af860eca5fc1be44f9d86be8a26e3c814d
Chua Hou <human+github@chuahou.dev>
2020-12-02 13:50:55 +0800

    day2: remove unnecessary data constructor

--- day2/day2.hs
+++ day2/day2.hs
@@ -5,11 +5,7 @@ import           Control.Monad (forM_)
 import           Data.Either   (fromRight)
 import           Text.Parsec
 
-data Requirement = Requirement { lowBound  :: Int
-                               , highBound :: Int
-                               , character :: Char
-                               , password  :: String
-                               }
+type Requirement = (Int, Int, Char, String)
 
 solve :: (Requirement -> Bool) -> [String] -> Int
 solve p = length . filter (fromRight False . parse (p <$> lineP) "")
@@ -19,16 +15,16 @@ lineP = do { low  <- read <$> many1 digit <* char '-'
            ; high <- read <$> many1 digit <* space
            ; c    <- anyChar              <* string ": "
            ; cs   <- many anyChar         <* eof
-           ; return $ Requirement low high c cs
+           ; return (low, high, c, cs)
            }
 
 check1 :: Requirement -> Bool
-check1 (Requirement low high c cs) =
+check1 (low, high, c, cs) =
     let cCount = length . filter (== c) $ cs
      in cCount <= high && cCount >= low
 
 check2 :: Requirement -> Bool
-check2 (Requirement x y c cs) =
+check2 (x, y, c, cs) =
     max x y <= length cs &&
         let cx = cs !! (x - 1)
             cy = cs !! (y - 1)
