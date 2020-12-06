65220d12c88141cb50f56e03a1ebed041b38ce85
Chua Hou <human+github@chuahou.dev>
2020-12-06 14:15:24 +0800

    day6: add part 1

---
+++ day6/day6.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 Chua Hou
+
+# part 1
+for grp in $(awk NF=NF RS= OFS= input); do
+	tr '\n' ' ' <<< $grp | sed 's/./&\n/g' | sort | uniq | sed '/^ $/d' | wc -l
+done | paste -sd+ | bc
