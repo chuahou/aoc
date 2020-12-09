60cd878e874f5a1f603b2088832f7f36cd2913b8
Chua Hou <human+github@chuahou.dev>
2020-12-09 14:08:06 +0800

    day9: add part 1

---
+++ day9/day9.hs
@@ -0,0 +1,30 @@
+-- SPDX-License-Identifier: MIT
+-- Copyright (c) 2020 Chua Hou
+
+import           Control.Monad (forM_)
+
+type ParsedInput = [Int]
+type Output      = Int
+
+type Window = [Int]
+
+validate :: Window -> Int -> Bool
+validate []     _ = False
+validate (w:ws) x = (x - w) `elem` ws || validate ws x
+
+parse :: String -> ParsedInput
+parse = map read . lines
+
+part1 :: ParsedInput -> Output
+part1 = uncurry part1' . splitAt 25
+    where part1' (w:ws) (x:xs) = if   validate (w:ws) x
+                                 then part1' (ws ++ [x]) xs
+                                 else x
+
+part2 :: ParsedInput -> Output
+part2 = undefined
+
+main :: IO ()
+main = do { input <- parse <$> readFile "input"
+          ; forM_ [part1, part2] (\p -> print . p $ input)
+          }
