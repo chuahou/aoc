e06a86007c434840b77356f4d715890f86944742
Chua Hou <human+github@chuahou.dev>
2020-12-09 14:21:13 +0800

    day9: add part 2

--- day9/day9.hs
+++ day9/day9.hs
@@ -12,6 +12,15 @@ validate :: Window -> Int -> Bool
 validate []     _ = False
 validate (w:ws) x = (x - w) `elem` ws || validate ws x
 
+findSet :: Int -> [Int] -> [Int] -> [Int]
+findSet _ _   [] = error "Could not find contiguous set"
+findSet y set (x:xs) | sumSet == y = if   length set >= 2
+                                     then set
+                                     else findSet y (tail set ++ [x]) xs
+                     | sumSet <  y = findSet y (set ++ [x]) xs
+                     | otherwise   = findSet y (tail set)   (x:xs)
+    where sumSet = sum set
+
 parse :: String -> ParsedInput
 parse = map read . lines
 
@@ -22,7 +31,8 @@ part1 = uncurry part1' . splitAt 25
                                  else x
 
 part2 :: ParsedInput -> Output
-part2 = undefined
+part2 xs = let set = findSet (part1 xs) [] xs
+            in minimum set + maximum set
 
 main :: IO ()
 main = do { input <- parse <$> readFile "input"
