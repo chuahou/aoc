e1db2e994334002a920681264d77bcbe04e00f9b
Chua Hou <human+github@chuahou.dev>
2020-12-11 21:48:46 +0800

    day11: further optimize by using @Set@s instead of lists

--- day11/Main.hs
+++ day11/Main.hs
@@ -7,18 +7,20 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE TupleSections #-}
 
-import           Control.Monad (forM_)
-import           Data.Map      (Map)
-import qualified Data.Map      as Map
-import           Data.Maybe    (fromMaybe, mapMaybe)
+import           Control.Monad   (forM_)
+import           Data.Map        (Map)
+import qualified Data.Map.Strict as Map
+import           Data.Maybe      (fromMaybe, mapMaybe)
+import           Data.Set        (Set)
+import qualified Data.Set        as Set
 
-type ParsedInput = ([Pos], (Int, Int))
+type ParsedInput = (Set Pos, (Int, Int))
 type Output      = Int
 
 type Pos  = (Int, Int)
 data Seat = Empty | Occupied deriving (Show, Eq)
 
-type Neighbours = Map Pos [Pos]
+type Neighbours = Map Pos (Set Pos)
 type State      = Map Pos Seat
 
 head' :: [a] -> Maybe a
@@ -29,20 +31,22 @@ fix :: Eq a => (a -> (a, Bool)) -> a -> a
 fix f x = let (x', b) = f x
            in if b then fix f x' else x
 
-neighbours :: [Pos] -> (Int, Int) -> Neighbours
-neighbours ps (h, w) = Map.fromList $ map neighbours' ps
-    where neighbours' (x, y) = ((x, y), [ (x', y')
-                                        | x' <- [x-1..x+1]
-                                        , x' >= 0 && x' < h
-                                        , y' <- [y-1..y+1]
-                                        , y' >= 0 && y' < w
-                                        , x' /= x || y' /= y
-                                        , (x', y') `elem` ps
-                                        ])
+neighbours :: Set Pos -> (Int, Int) -> Neighbours
+neighbours ps (h, w) = Map.fromList . Set.toList $ Set.map neighbours' ps
+    where neighbours' (x, y) = ((x, y), Set.fromList [ (x', y')
+                                                     | x' <- [x-1..x+1]
+                                                     , x' >= 0 && x' < h
+                                                     , y' <- [y-1..y+1]
+                                                     , y' >= 0 && y' < w
+                                                     , x' /= x || y' /= y
+                                                     , (x', y') `Set.member` ps
+                                                     ])
 
-visible :: [Pos] -> (Int, Int) -> Neighbours
-visible ps (h, w) = Map.fromList $ map visible' ps
-    where visible' (x, y) = ((x, y), mapMaybe (visible'' (x, y)) dirns)
+visible :: Set Pos -> (Int, Int) -> Neighbours
+visible ps (h, w) = Map.fromList . Set.toList $ Set.map visible' ps
+    where visible' (x, y) = ( (x, y)
+                            , Set.fromList . mapMaybe (visible'' (x, y)) $ dirns
+                            )
           dirns = [ (dx, dy)
                   | dx <- [-1..1]
                   , dy <- [-1..1]
@@ -50,12 +54,12 @@ visible ps (h, w) = Map.fromList $ map visible' ps
                   ]
           visible'' (x', y') (dx, dy)
             | x' < 0 || x' > h || y' < 0 || y' > w = Nothing
-            | (x' + dx, y' + dy) `elem` ps         = Just (x'+dx, y'+dy)
+            | (x' + dx, y' + dy) `Set.member` ps   = Just (x'+dx, y'+dy)
             | otherwise                            = visible'' (x'+dx, y'+dy)
                                                                (dx, dy)
 
-initState :: [Pos] -> State
-initState ps = Map.fromList $ map (, Empty) ps
+initState :: Set Pos -> State
+initState = Map.fromSet (const Empty)
 
 step :: Int -> Neighbours -> State -> (State, Bool)
 step threshold ns = fromMaybe (error "Failed to step") . step' threshold ns
@@ -63,10 +67,10 @@ step threshold ns = fromMaybe (error "Failed to step") . step' threshold ns
 step' :: Int -> Neighbours -> State -> Maybe (State, Bool)
 step' threshold ns ss = Map.foldrWithKey c (Just (Map.empty, False)) ss
     where c key st mb = do
-            { nsStates <-  filter (== Occupied)
-                       .   mapMaybe (`Map.lookup` ss)
-                       <$> Map.lookup key ns
-            ; (m, b) <- mb
+            { (m, b) <- mb
+            ; ns'    <- Map.lookup key ns
+            ; let nsStates = Map.filter (== Occupied)
+                           $ Map.intersection ss (Map.fromSet id ns')
             ; let (st', b') = case st of
                                 Occupied -> if length nsStates >= threshold
                                                then (Empty, True)
@@ -81,11 +85,11 @@ parse :: String -> Maybe ParsedInput
 parse cs = do { let ls = lines cs
               ; let h  = length ls
               ; w  <- length <$> head' ls
-              ; ss <- map fst
-                   <$> (foldr filtSeat (Just []) . concat . enumerate) ls
+              ; ss <- Set.map fst
+                   <$> (foldr filtSeat (Just Set.empty) . concat . enumerate) ls
               ; return (ss, (h, w))
               }
-    where filtSeat (p, 'L') ys = ((p, 'L'):) <$> ys
+    where filtSeat (p, 'L') ys = Set.insert (p, 'L') <$> ys
           filtSeat (_, '.') ys = ys
           filtSeat _        _  = Nothing
           enumerate = zipWith (\i -> zip (map (i,) [0..])) [0..]
