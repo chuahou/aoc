89e2236390b21fed697a9a7064c69380b0b27fe1
Chua Hou <human+github@chuahou.dev>
2020-12-11 17:58:10 +0800

    day11: switch to Data.Vector

--- day11/Main.hs
+++ day11/Main.hs
@@ -5,36 +5,32 @@
 
 import           Control.Monad (forM_)
 import           Data.Maybe    (fromMaybe, mapMaybe)
+import           Data.Vector   (Vector, (!?))
+import qualified Data.Vector   as V
 
-type ParsedInput = Grid Cell
+type ParsedInput = Vector (Vector Cell)
 type Output      = Int
 
 data Cell = Floor | Empty | Occupied deriving Eq
 
-data Grid a = Grid Int Int [[a]] deriving Eq
+type Grid a = Vector (Vector a)
 
 instance Show Cell where
     show Floor    = "."
     show Empty    = "L"
     show Occupied = "#"
 
-instance Show a => Show (Grid a) where
-    show (Grid _ _ css) = unlines . map (concatMap show) $ css
-
-toGrid :: [[a]] -> Grid a
-toGrid xss = let h = length xss
-                 w = minimum . map length $ xss -- can't handle infinite!
-              in Grid h w (map (take w) xss)
-
 (<!!>) :: Grid a -> (Int, Int) -> Maybe a
-(Grid h w css) <!!> (i, j)
-    | i < h && i >= 0 && j < w && j >= 0 = Just $ css !! i !! j
-    | otherwise                          = Nothing
+g <!!> (i, j) = g !? i >>= (!? j)
+
+fromList :: [[a]] -> Grid a
+fromList []       = V.fromList []
+fromList (xs:xss) = let w = minimum . map length $ xs:xss
+                     in V.fromList . map (V.fromList . take w) $ (xs:xss)
 
 neighbours :: Grid a -> (Int, Int) -> [a]
-neighbours g (i, j) = mapMaybe (g <!!>) indices
-    where
-        indices = [ (x, y) | x <- [i-1..i+1], y <-[j-1..j+1], x /= i || y /= j ]
+neighbours g (i, j) = mapMaybe (g <!!>)
+        [ (x, y) | x <- [i-1..i+1], y <-[j-1..j+1], x /= i || y /= j ]
 
 rays :: Grid Cell -> (Int, Int) -> [Cell]
 rays g ij = mapMaybe (cast ij) directions
@@ -48,14 +44,15 @@ rays g ij = mapMaybe (cast ij) directions
                    Just Floor    -> m
                    Nothing       -> Nothing
 
-gridIndices :: Grid a -> [[(Int, Int)]]
-gridIndices (Grid h w _) = [ [(x, y) | y <- [0..w-1] ] | x <- [0..h-1] ]
+indices :: Grid a -> [[(Int, Int)]]
+indices xss = [ [ (x, y) | y <- [0..(V.length (V.head xss) - 1)] ]
+                         | x <- [0..(V.length xss - 1)] ]
 
-gridFilter :: (a -> Bool) -> Grid a -> [a]
-gridFilter p (Grid _ _ css) = concatMap (filter p) css
+countP :: (a -> Bool) -> Grid a -> Int
+countP p = V.length . V.concatMap (V.filter p)
 
 step :: (Grid Cell -> (Int, Int) -> Int) -> Int -> Grid Cell -> Grid Cell
-step count threshold g = toGrid . map (map stepCell) $ indices g
+step count threshold g = fromList . map (map stepCell) $ indices g
     where stepCell (i, j) =
             case fromMaybe (error "Indexing error") $ g <!!> (i, j) of
               Floor    -> Floor
@@ -63,23 +60,22 @@ step count threshold g = toGrid . map (map stepCell) $ indices g
                           then Empty else Occupied
               Occupied -> if count g (i, j) >= threshold
                           then Empty else Occupied
-          indices (Grid h w _) = [ [ (x, y) | y <- [0..w-1] ] | x <- [0..h-1] ]
 
 fix :: (Grid Cell -> Grid Cell) -> Grid Cell -> Grid Cell
 fix f g = let g' = f g in if g == g' then g' else fix f g'
 
 parse :: String -> Maybe ParsedInput
-parse = fmap toGrid . mapM (mapM cell) . lines
+parse = fmap fromList . mapM (mapM cell) . lines
     where cell 'L' = Just Empty
           cell '.' = Just Floor
           cell _   = Nothing
 
 part1 :: ParsedInput -> Output
-part1 = length . gridFilter (== Occupied) . fix (step count 4)
+part1 = countP (== Occupied) . fix (step count 4)
     where count g = length . filter (== Occupied) . neighbours g
 
 part2 :: ParsedInput -> Output
-part2 = length . gridFilter (== Occupied) . fix (step count 5)
+part2 = countP (== Occupied) . fix (step count 5)
     where count g = length . filter (== Occupied) . rays g
 
 main :: IO ()
