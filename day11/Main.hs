dbeeda3ecd578822ac0a89a8c01345590ecd6862
Chua Hou <human+github@chuahou.dev>
2020-12-11 20:33:55 +0800

    day11: redo part 1 in @Map@ with inspiration from mstksg

--- day11/Main.hs
+++ day11/Main.hs
@@ -1,85 +1,91 @@
 -- SPDX-License-Identifier: MIT
 -- Copyright (c) 2020 Chua Hou
+--
+-- Refactored solution below inspired by
+-- https://github.com/mstksg/advent-of-code-2020
 
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE TupleSections #-}
 
 import           Control.Monad (forM_)
+import           Data.Map      (Map)
+import qualified Data.Map      as Map
 import           Data.Maybe    (fromMaybe, mapMaybe)
-import           Data.Vector   (Vector, (!?))
-import qualified Data.Vector   as V
 
-type ParsedInput = Vector (Vector Cell)
+type ParsedInput = ([Pos], (Int, Int))
 type Output      = Int
 
-data Cell = Floor | Empty | Occupied deriving Eq
+type Pos  = (Int, Int)
+data Seat = Empty | Occupied deriving (Show, Eq)
 
-type Grid a = Vector (Vector a)
+type Neighbours = Map Pos [Pos]
+type State      = Map Pos Seat
 
-instance Show Cell where
-    show Floor    = "."
-    show Empty    = "L"
-    show Occupied = "#"
+head' :: [a] -> Maybe a
+head' []    = Nothing
+head' (x:_) = Just x
 
-(<!!>) :: Grid a -> (Int, Int) -> Maybe a
-g <!!> (i, j) = g !? i >>= (!? j)
+fix :: Eq a => (a -> a) -> a -> a
+fix f x = let x' = f x
+           in if x == x' then x else fix f x'
 
-fromList :: [[a]] -> Grid a
-fromList []       = V.fromList []
-fromList (xs:xss) = let w = minimum . map length $ xs:xss
-                     in V.fromList . map (V.fromList . take w) $ (xs:xss)
+neighbours :: [Pos] -> (Int, Int) -> Neighbours
+neighbours ps (h, w) = Map.fromList $ map neighbours' ps
+    where neighbours' (x, y) = ((x, y), [ (x', y')
+                                        | x' <- [x-1..x+1]
+                                        , x' >= 0 && x' < h
+                                        , y' <- [y-1..y+1]
+                                        , y' >= 0 && y' < w
+                                        , x' /= x || y' /= y
+                                        , (x', y') `elem` ps
+                                        ])
 
-neighbours :: Grid a -> (Int, Int) -> [a]
-neighbours g (i, j) = mapMaybe (g <!!>)
-        [ (x, y) | x <- [i-1..i+1], y <-[j-1..j+1], x /= i || y /= j ]
+initState :: [Pos] -> State
+initState ps = Map.fromList $ map (, Empty) ps
 
-rays :: Grid Cell -> (Int, Int) -> [Cell]
-rays g ij = mapMaybe (cast ij) directions
-    where
-        directions = [ (x, y) | x <- [-1..1], y <- [-1..1], x /= 0 || y /= 0 ]
-        cast (i0, j0) (di, dj) = foldr c Nothing $
-            [ (i0 + n * di, j0 + n * dj) | n <- [1..] ]
-        c ij' m = case g <!!> ij' of
-                   Just Empty    -> Nothing
-                   Just Occupied -> Just Occupied
-                   Just Floor    -> m
-                   Nothing       -> Nothing
+step :: Int -> Neighbours -> State -> State
+step threshold ns = fromMaybe (error "Failed to step") . step' threshold ns
 
-indices :: Grid a -> [[(Int, Int)]]
-indices xss = [ [ (x, y) | y <- [0..(V.length (V.head xss) - 1)] ]
-                         | x <- [0..(V.length xss - 1)] ]
-
-countP :: (a -> Bool) -> Grid a -> Int
-countP p = V.length . V.concatMap (V.filter p)
-
-step :: (Grid Cell -> (Int, Int) -> Int) -> Int -> Grid Cell -> Grid Cell
-step count threshold g = fromList . map (map stepCell) $ indices g
-    where stepCell (i, j) =
-            case fromMaybe (error "Indexing error") $ g <!!> (i, j) of
-              Floor    -> Floor
-              Empty    -> if count g (i, j) > 0
-                          then Empty else Occupied
-              Occupied -> if count g (i, j) >= threshold
-                          then Empty else Occupied
-
-fix :: (Grid Cell -> Grid Cell) -> Grid Cell -> Grid Cell
-fix f g = let g' = f g in if g == g' then g' else fix f g'
+step' :: Int -> Neighbours -> State -> Maybe State
+step' threshold ns ss = Map.foldrWithKey c (Just Map.empty) ss
+    where c :: Pos -> Seat -> Maybe State -> Maybe State
+          c key st y = do
+            { nsStates <-  filter (== Occupied)
+                       .   mapMaybe (`Map.lookup` ss)
+                       <$> Map.lookup key ns
+            ; let st' = case st of
+                          Occupied -> if length nsStates >= threshold
+                                         then Empty
+                                         else Occupied
+                          Empty    -> if null nsStates
+                                         then Occupied
+                                         else Empty
+            ; Map.insert key st' <$> y
+            }
 
 parse :: String -> Maybe ParsedInput
-parse = fmap fromList . mapM (mapM cell) . lines
-    where cell 'L' = Just Empty
-          cell '.' = Just Floor
-          cell _   = Nothing
+parse cs = do { let ls = lines cs
+              ; let h  = length ls
+              ; w  <- length <$> head' ls
+              ; ss <- map fst
+                   <$> (foldr filtSeat (Just []) . concat . enumerate) ls
+              ; return (ss, (h, w))
+              }
+    where filtSeat (p, 'L') ys = ((p, 'L'):) <$> ys
+          filtSeat (_, '.') ys = ys
+          filtSeat _        _  = Nothing
+          enumerate = zipWith (\i -> zip (map (i,) [0..])) [0..]
 
 part1 :: ParsedInput -> Output
-part1 = countP (== Occupied) . fix (step count 4)
-    where count g = length . filter (== Occupied) . neighbours g
+part1 (ps, (w, h)) = let ns = neighbours ps (w, h)
+                         st = fix (step 4 ns) (initState ps)
+                      in length . Map.filter (== Occupied) $ st
 
 part2 :: ParsedInput -> Output
-part2 = countP (== Occupied) . fix (step count 5)
-    where count g = length . filter (== Occupied) . rays g
+part2 = undefined
 
 main :: IO ()
-main = do { input <- fromMaybe (error "Parse error") . parse
-                      <$> readFile "input"
+main = do { input <-  fromMaybe (error "Parse error") . parse
+                  <$> readFile "input"
           ; forM_ [part1, part2] (\p -> print . p $ input)
           }
