9478cf7a47efabb2abc406b30be790615efc6df1
Chua Hou <human+github@chuahou.dev>
2020-12-11 23:05:58 +0800

    template: make parsing return @Maybe@

--- template/Main.hs
+++ template/Main.hs
@@ -1,12 +1,15 @@
 -- SPDX-License-Identifier: MIT
 -- Copyright (c) 2020 Chua Hou
 
+{-# OPTIONS_GHC -Wall #-}
+
 import           Control.Monad (forM_)
+import           Data.Maybe    (fromMaybe)
 
 type ParsedInput = [Int]
 type Output      = Int
 
-parse :: String -> ParsedInput
+parse :: String -> Maybe ParsedInput
 parse = undefined
 
 part1 :: ParsedInput -> Output
@@ -16,6 +19,7 @@ part2 :: ParsedInput -> Output
 part2 = undefined
 
 main :: IO ()
-main = do { input <- parse <$> readFile "input"
+main = do { input <-  fromMaybe (error "Parse error") . parse
+                  <$> readFile "input"
           ; forM_ [part1, part2] (\p -> print . p $ input)
           }
