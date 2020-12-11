2ee190aebe73d131c0e104ffad9d15c692a46993
Chua Hou <human+github@chuahou.dev>
2020-12-12 01:07:17 +0800

    day11: improve efficiency by storing neighbourhood maps

--- day11/Main.hs
+++ day11/Main.hs
@@ -20,35 +20,41 @@ type ParsedInput = State
 type Output      = Int
 
 type Pos   = (Int, Int)
+type Sets  = Map Pos [Pos]
 type State = Map Pos Seat
 data Seat  = Empty | Occupied | Floor deriving Eq
 
 fix :: Eq a => (a -> a) -> a -> a
 fix f x = let x' = f x in if x == x' then x else fix f x'
 
-step :: Int -> (State -> Pos -> [Seat]) -> State -> State
-step threshold getSet state = Map.mapWithKey step' state
+step :: Int -> Sets -> State -> State
+step threshold sets state = Map.mapWithKey step' state
     where
-        step' pos Empty    = if Occupied `notElem` getSet state pos
+        step' pos Empty    = if Occupied `notElem` set pos
                                 then Occupied else Empty
         step' pos Occupied = if not . null . drop (threshold - 1)
-                                . filter (== Occupied) $ getSet state pos
+                                . filter (== Occupied) $ set pos
                                 then Empty else Occupied
         step' _   Floor    = Floor
+        set pos = maybe (error "Invalid seat") (mapMaybe (state !?))
+                $ sets !? pos
 
-neighbours :: State -> Pos -> [Seat]
-neighbours s (x, y) = mapMaybe (s !?) [ (x + dx, y + dy)
-                                      | (dx, dy) <- directions
-                                      ]
+makeSets :: (State -> Pos -> [Pos]) -> State -> Sets
+makeSets f s = Map.mapWithKey (const . f s) s
 
-visible :: State -> Pos -> [Seat]
+neighbours :: State -> Pos -> [Pos]
+neighbours s (x, y) = filter (`Map.member` s) [ (x + dx, y + dy)
+                                              | (dx, dy) <- directions
+                                              ]
+
+visible :: State -> Pos -> [Pos]
 visible s (x, y) = mapMaybe visible' directions
     where
         visible' (dx, dy) = visible'' (x + dx, y + dy) (dx, dy)
         visible'' (x', y') (dx, dy) =
             case s !? (x', y') of
-              Just Occupied -> Just Occupied
-              Just Empty    -> Nothing
+              Just Occupied -> Just (x', y')
+              Just Empty    -> Just (x', y')
               Just Floor    -> visible'' (x' + dx, y' + dy) (dx, dy)
               Nothing       -> Nothing
 
@@ -63,16 +69,20 @@ parse = fmap Map.fromList . mapM readSeat . enumerate . lines
         readSeat (p, '.') = Just (p, Floor)
         readSeat (_, _)   = Nothing
 
+solve :: Int -> (State -> Pos -> [Pos]) -> State -> Int
+solve n f s = Map.size . Map.filter (== Occupied)
+            . fix (step n (makeSets f s)) $ Map.filter (/= Floor) s
+
 part1 :: ParsedInput -> Output
-part1 = Map.size . Map.filter (== Occupied) . fix (step 4 neighbours)
+part1 = solve 4 neighbours
 
 part2 :: ParsedInput -> Output
-part2 = Map.size . Map.filter (== Occupied) . fix (step 5 visible)
+part2 = solve 5 visible
 
-solve :: String -> IO ()
-solve s = do { input <-  fromMaybe (error "Parse error") . parse <$> readFile s
-             ; forM_ [part1, part2] (\p -> print . p $ input)
-             }
+runFile :: String -> IO ()
+runFile s = do { input <- fromMaybe (error "Parse error") . parse <$> readFile s
+               ; forM_ [part1, part2] (\p -> print . p $ input)
+               }
 
 main :: IO ()
-main = solve "input"
+main = runFile "input"
