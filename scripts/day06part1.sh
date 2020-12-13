95af9c65c6f79f5e19fac0bfa253bbd5d71c84a5
Chua Hou <human+github@chuahou.dev>
2020-12-14 05:01:16 +0800

    old: convert day 6 to new format

--- scripts/day06part1.sh
+++ scripts/day06part1.sh
@@ -3,6 +3,6 @@
 # Copyright (c) 2020 Chua Hou
 
 # part 1
-for grp in $(awk NF=NF RS= OFS= input); do
+for grp in $(awk NF=NF RS= OFS= input/day06.txt); do
 	tr '\n' ' ' <<< $grp | sed 's/./&\n/g' | sort | uniq | sed '/^ $/d' | wc -l
 done | paste -sd+ | bc
--- src/AOC/Days/Day06.hs
+++ src/AOC/Days/Day06.hs
@@ -5,8 +5,21 @@ module AOC.Days.Day06 (solution) where
 
 import           AOC.Solution
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
-    undefined -- part2
+group :: String -> [String]
+group = group' ""
+
+group' :: String -> String -> [String]
+group' ys []             = [reverse ys]
+group' ys ('\n':'\n':xs) = reverse ys : group' "" xs
+group' ys (x:xs)         = group' (x:ys) xs
+
+count :: [String] -> Int
+count (cs:css) = length . foldr (filter . flip elem) cs $ css
+count []       = 0
+
+solution :: String :=> String
+solution = Solution
+    Just
+    (const "Run script scripts/day06part1.sh directly")
+    (show . sum . map (count . lines) . group)
+    id
