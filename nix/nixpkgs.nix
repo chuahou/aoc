d54aa50365da99ea80ef312cde5283abed5dedb8
Chua Hou <human+github@chuahou.dev>
2020-12-13 23:36:53 +0800

    nix: initial setup

---
+++ nix/nixpkgs.nix
@@ -0,0 +1,6 @@
+import (builtins.fetchGit {
+  name = "nixos-20.09-2020-12-13";
+  url  = "https://github.com/nixos/nixpkgs/";
+  ref  = "refs/heads/nixos-20.09";
+  rev  = "65c9cc79f1d179713c227bf447fb0dac384cdcda";
+})
