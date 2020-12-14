0bdd0e3e7110de651a67ecbea66bae249160e0f8
Chua Hou <human+github@chuahou.dev>
2020-12-14 15:38:40 +0800

    day14: add part 2

--- src/AOC/Days/Day14.hs
+++ src/AOC/Days/Day14.hs
@@ -5,6 +5,7 @@ module AOC.Days.Day14 (solution) where
 
 import           Data.Bits          (complement, (.&.), (.|.))
 import           Data.Foldable      (foldl')
+import           Data.List          (nub)
 import           Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NE
 import           Data.Map           (Map)
@@ -21,6 +22,7 @@ import           AOC.Solution
 type Value    = Integer
 type Mask     = NonEmpty MaskChar
 data MaskChar = X | M1 | M0 deriving Show
+data AddrChar = AX | A1 | A0 deriving Show
 data Write    = Write Mask Int Value deriving Show
 type Memory   = Map Int Value
 
@@ -36,8 +38,29 @@ applyMask m = (.&.) (complement l) . (.|.) h
     where
         (h, l) = valMask m
 
+addrMask :: Mask -> Int -> [Int]
+addrMask m = nub . addrMask' . mask (NE.reverse m)
+    where
+        possible AX = [0, 1]
+        possible A0 = [0]
+        possible A1 = [1]
+        addrMask' (x:|[])   = possible x
+        addrMask' (x:|y:ys) = [ y' * 2 + x'
+                              | x' <- possible x
+                              , y' <- addrMask' (y:|ys)
+                              ]
+        mask (x:|[])   a = maskBit x a :| []
+        mask (x:|y:ys) a = maskBit x a :| NE.toList (mask (y:|ys) (a `div` 2))
+        maskBit X  _ = AX
+        maskBit M0 x = if x `mod` 2 == 1 then A1 else A0
+        maskBit M1 _ = A1
+
 applyWrite :: Write -> Memory -> Memory
 applyWrite (Write m addr val) = Map.insert addr (applyMask m val)
+
+applyWrite' :: Write -> Memory -> Memory
+applyWrite' (Write m addr val) =
+    Map.union (Map.fromList (map (,val) (addrMask m addr)))
 
 ----- PARSING -----
 
@@ -74,4 +97,4 @@ solution :: [Write] :=> Value
 solution = simpleSolution
     (fromParsec inputP)
     (sum . Map.elems . foldl' (flip applyWrite) Map.empty)
-    undefined -- part2
+    (sum . Map.elems . foldl' (flip applyWrite') Map.empty)
