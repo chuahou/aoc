499468bbd6976af9ecf548817819989aab6eb1b0
Chua Hou <human+github@chuahou.dev>
2020-12-14 14:46:07 +0800

    AOC.Solution: add helper methods
    - fromParsec to create parser from parsec parser
    - test to quickly run solution on given input file

--- src/AOC/Solution.hs
+++ src/AOC/Solution.hs
@@ -4,10 +4,15 @@
 module AOC.Solution ( (:=>) (Solution, Script)
                     , simpleSolution
                     , runSolution
+                    , fromParsec
+                    , test
                     ) where
 
-import           System.IO      (FilePath)
-import           System.Process (callProcess)
+import           Data.Maybe         (fromMaybe)
+import           System.IO          (FilePath)
+import           System.Process     (callProcess)
+import qualified Text.Parsec        as P
+import           Text.Parsec.String (Parser)
 
 import           Paths_aoc
 
@@ -27,3 +32,9 @@ runSolution :: a :=> b -> String -> Maybe (IO ())
 runSolution (Solution p p1 p2 ps) x = p x >>= \x' ->
     return . putStrLn . unlines . map (\part -> ps . part $ x') $ [p1, p2]
 runSolution (Script s) _ = Just $ getDataFileName s >>= flip callProcess []
+
+fromParsec :: Parser a -> (String -> Maybe a)
+fromParsec p = either (const Nothing) Just . P.parse p ""
+
+test :: a :=> b -> FilePath -> IO ()
+test s f = readFile f >>= fromMaybe (fail "Failed to run") . runSolution s
