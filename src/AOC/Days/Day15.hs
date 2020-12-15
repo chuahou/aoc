ce132c998f5a1e2e556ea2fd3cee943134207b54
Chua Hou <human+github@chuahou.dev>
2020-12-15 17:37:41 +0800

    day15: use mutable vector for part 2 (still bad)
    - 48s -> 30s, at least

--- src/AOC/Days/Day15.hs
+++ src/AOC/Days/Day15.hs
@@ -3,36 +3,36 @@
 
 module AOC.Days.Day15 (solution) where
 
-import           Data.IntMap.Strict (IntMap)
-import qualified Data.IntMap.Strict as Map
-import           Data.List.NonEmpty (NonEmpty (..))
-import qualified Data.List.NonEmpty as NE
+import           Control.Monad       (forM_)
+import           Control.Monad.ST
+import           Data.Foldable       (foldlM)
+import           Data.List.NonEmpty  (NonEmpty (..))
+import qualified Data.List.NonEmpty  as NE
+import qualified Data.Vector.Mutable as VM
 
 import           AOC.Parsec
 import           AOC.Solution
 
-genNums :: NonEmpty Int -> Int -> Maybe Int
-genNums input target
-    | target <= 0                   = Nothing
-    | target <= NE.length input     = Just $ input NE.!! (target - 1)
-    | target == NE.length input + 1 = Just 0
-    | otherwise                     = Just $ genInput input Map.empty 1
-    where
-        genInput (x:|y:ys) m i = genInput (y:|ys) (Map.insert x i m) (i + 1)
-        genInput (x:|[])   m i = genNums' (Map.insert x i m) (i + 1) 0 target
+run :: NonEmpty Int -> Int -> Maybe Int
+run input target
+    | target <= 0            = Nothing
+    | target <= length input = Just $ input NE.!! (target - 1)
+    | otherwise = let len = length input
+                   in Just $ runST $ do
+        { v <- VM.replicate (maximum (target : NE.toList input) + 1) 0
+        ; forM_ (zip (NE.init input) [1..]) $ uncurry (VM.write v)
+        ; foldlM (speakNum v) (NE.last input) [len..target-1]
+        }
 
--- @genNums' m i prev@ calculates the @i+1@th element, where @prev@ is the @i@th
--- element and @m@ contains the last spoken of each number before the @i@th
--- element. It returns the previous element when @i == target@.
-genNums' :: IntMap Int -> Int -> Int -> Int -> Int
-genNums' !m !i !prev target
-    | i == target = prev
-    | otherwise   = genNums' (Map.insert prev i m) (i + 1) next target
-    where
-        next = maybe 0 (i -) $ Map.lookup prev m
+speakNum :: VM.MVector s Int -> Int -> Int -> ST s Int
+speakNum !v !prev i = do
+    { prevPos <- VM.unsafeRead v prev
+    ; VM.write v prev i
+    ; return $ if prevPos == 0 then 0 else i - prevPos
+    }
 
 solution :: NonEmpty Int :=> Maybe Int
 solution = simpleSolution
     (fromParsec $ nonEmptyP $ sepBy1 (readP (many1 digit)) (char ','))
-    (`genNums` 2020)
-    (`genNums` 30000000)
+    (`run` 2020)
+    (`run` 30000000)
