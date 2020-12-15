684c66696c4612705a7727ccad3594c05b422058
Chua Hou <human+github@chuahou.dev>
2020-12-16 03:57:48 +0800

    day9: fix

--- src/AOC/Days/Day09.hs
+++ src/AOC/Days/Day09.hs
@@ -3,9 +3,6 @@
 
 module AOC.Days.Day09 (solution) where
 
-import           Data.List.NonEmpty (NonEmpty (..))
-import qualified Data.List.NonEmpty as NE
-
 import           AOC.Solution
 
 type Window = [Int]
@@ -14,23 +11,20 @@ validate :: Window -> Int -> Bool
 validate []     _ = False
 validate (w:ws) x = (x - w) `elem` ws || validate ws x
 
-findSet :: Int -> Int -> NonEmpty Int -> [Int] -> Maybe [Int]
+findSet :: Int -> Int -> [Int] -> [Int] -> Maybe [Int]
 findSet _ _   _   [] = Nothing
 findSet y sum' set (x:xs)
     | sum' == y = if   length set >= 2
-                  then Just $ NE.toList set
-                  else findSet y (sum' - NE.head set + x)
-                                 (NE.tail set <+|+> x)
-                                 xs
-    | sum' <  y = findSet y (sum' + x) (set <++> x) xs
-    | otherwise = case NE.tail set of
-                    (t:ts) -> findSet y (sum' - NE.head set) (t:|ts) (x:xs)
-                    []     -> findSet y (sum' + x)           (x:|[]) xs
+                  then Just set
+                  else expand
+    | sum' <  y = expand
+    | otherwise = shrink
     where
-        appendNE  xs' x' = NE.reverse $ x' :| reverse xs'
-        appendNE' ne  x' = appendNE (NE.toList ne) x'
-        (<+|+>) = appendNE
-        (<++>)  = appendNE'
+        expand = findSet y (sum' + x) (set ++ [x]) xs
+        shrink = case set of
+                   (s:ss) -> findSet y (sum' - s) ss (x:xs)
+                   []     -> expand -- can't shrink
+
 
 part1 :: [Int] -> Maybe Int
 part1 = uncurry part1' . splitAt 25
@@ -41,9 +35,7 @@ part1 = uncurry part1' . splitAt 25
 
 part2 :: [Int] -> Maybe Int
 part2 xs = do { inval  <- part1 xs
-              ; x      <- head xs
-              ; xs'    <- tail xs
-              ; set    <- findSet inval 0 (x:|[]) xs'
+              ; set    <- findSet inval 0 [] xs
               ; minSet <- minimum set
               ; maxSet <- maximum set
               ; return $ minSet + maxSet
