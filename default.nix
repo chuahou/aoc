9f7170c860e59bcf129ddfcf78e2d89d6a054ac4
Chua Hou <human+github@chuahou.dev>
2020-12-17 02:45:38 +0800

    nix: move cabal-install to shell.nix
    - nix doesn't actually need cabal in 'default.nix' to perform default build

--- default.nix
+++ default.nix
@@ -1,10 +1,3 @@
 { pkgs ? import ./nix/nixpkgs.nix { config = import ./nix/config.nix; } }:
 
-let
-  aoc = pkgs.haskellPackages.callCabal2nix "aoc" ./. {};
-in
-  pkgs.haskell.lib.overrideCabal aoc (old: {
-    buildTools = (old.buildTools or []) ++ [
-      pkgs.haskellPackages.cabal-install
-    ];
-  })
+pkgs.haskellPackages.callCabal2nix "aoc" ./. {}
