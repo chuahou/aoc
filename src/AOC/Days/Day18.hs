0b9811220b4ce569946bdcebd5979e8b95fa66e0
Chua Hou <human+github@chuahou.dev>
2020-12-18 16:10:23 +0800

    day18: add part 1

--- src/AOC/Days/Day18.hs
+++ src/AOC/Days/Day18.hs
@@ -3,10 +3,30 @@
 
 module AOC.Days.Day18 (solution) where
 
+import           Control.Monad (void)
+
+import           AOC.Parsec
 import           AOC.Solution
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
+data Expr = Atom Int | Add Expr Expr | Times Expr Expr deriving Show
+
+wsP :: Parser ()
+wsP = void $ many (char ' ' <|> endOfLine)
+
+exprP :: Parser Expr
+exprP = chainl1 (termP <* wsP) (opP <* wsP)
+    where
+        termP =   Atom <$> readP (many1 digit)
+              <|> char '(' *> exprP <* char ')'
+        opP = (char '*' >> return Times) <|> (char '+' >> return Add)
+
+eval :: Expr -> Int
+eval (Atom x)    = x
+eval (Add x y)   = eval x + eval y
+eval (Times x y) = eval x * eval y
+
+solution :: [Expr] :=> Int
+solution = simpleSolution
+    (fromParsec $ many exprP)
+    (sum . map eval)
     undefined -- part2
