0bc2af4ed032e99b924c7475dbfaa5537f4d9a2f
Chua Hou <human+github@chuahou.dev>
2020-12-20 18:34:33 +0800

    day20: add dirty solution for part 1

--- src/AOC/Days/Day20.hs
+++ src/AOC/Days/Day20.hs
@@ -3,10 +3,69 @@
 
 module AOC.Days.Day20 (solution) where
 
+import           AOC.Parsec
 import           AOC.Solution
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
+----- SOLUTION -----
+
+-- borders are E, S, W, N, with left-to-right in list = clockwise
+data Tile   = Tile Int Border Border Border Border deriving Show
+type Border = [Cell]
+data Cell   = Hash | Dot deriving Eq
+
+instance Show Cell where
+    show Hash = "#"
+    show Dot  = "."
+
+(~=) :: Border -> Border -> Bool
+x ~= y = x == y || reverse x == y
+
+borders :: Tile -> [Border]
+borders (Tile _ b1 b2 b3 b4) = [b1, b2, b3, b4]
+
+canNeighbour :: Border -> Tile -> Bool
+canNeighbour b t = any (b ~=) $ borders t
+
+findCorners :: [Tile] -> [Tile]
+findCorners ts = go ts
+    where
+        go [] = []
+        go (t:ts')
+            | (length . filter hasNeighbour . borders) t == 2 = t : go ts'
+            | otherwise                                       = go ts'
+        hasNeighbour b = (length . filter (canNeighbour b)) ts > 1
+
+----- PARSING -----
+
+cellP :: Parser Cell
+cellP = (char '#' >> return Hash) <|> (char '.' >> return Dot)
+
+tileP :: Parser Tile
+tileP = do
+    { num <- string "Tile " *> readP (many1 digit) <* char ':' <* endOfLine
+    ; pic <- count 10 (count 10 cellP <* endOfLine)
+    ; (e, s, w, n) <- case getBorders pic of
+                        Just bs -> return bs
+                        Nothing -> fail "invalid pic"
+    ; return $ Tile num e s w n
+    }
+    where
+        getBorders pic = do
+            { e <- mapM last pic
+            ; s <- last pic
+            ; w <- reverse <$> mapM head pic
+            ; n <- reverse <$> head pic
+            ; return (e, s, w, n)
+            }
+
+----- SKELETON -----
+
+solution :: [Tile] :=> Maybe Int
+solution = simpleSolution
+    (fromParsec (sepBy1 tileP endOfLine <* eof))
+    (\ts ->
+        case findCorners ts of
+          [Tile a _ _ _ _, Tile b _ _ _ _, Tile c _ _ _ _, Tile d _ _ _ _] ->
+              Just $ product [a, b, c, d]
+          _ -> Nothing)
     undefined -- part2
