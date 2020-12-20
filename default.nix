9eb7531b555b6f6441996d1a814797f24ba9bd73
Chua Hou <human+github@chuahou.dev>
2020-12-20 14:24:17 +0800

    nix, envrc: use lorri
    - envrc uses lorri if present
    - lorri doesn't support IFD properly so 'deepSeq' is used to forcibly watch package.yaml

--- default.nix
+++ default.nix
@@ -1,3 +1,5 @@
 { pkgs ? import ./nix/nixpkgs.nix { config = import ./nix/config.nix; } }:
 
-pkgs.haskellPackages.callCabal2nix "aoc" ./. {}
+builtins.deepSeq
+  (builtins.readFile ./package.yaml)
+  (pkgs.haskellPackages.callCabal2nix "aoc" ./. {})
