f46b3a511693d5158810c2fa96543325f1665a5d
Chua Hou <human+github@chuahou.dev>
2020-12-22 01:15:21 +0800

    nix: use 'filterSource' instead of 'fetchGit' on './.'
    - The 'fetchGit' workaround is nice but doesn't work on CI as CI makes
      shallow clone of current repository

--- default.nix
+++ default.nix
@@ -1,5 +1,11 @@
 { pkgs ? import ./nix/nixpkgs.nix { config = import ./nix/config.nix; } }:
 
-builtins.deepSeq
-  (builtins.readFile ./package.yaml)
-  (pkgs.haskellPackages.callCabal2nix "aoc" (builtins.fetchGit ./.) {})
+let
+  src = builtins.filterSource (path: type:
+    (type != "directory" || baseNameOf path != ".dist-newstyle") &&
+    (type != "symlink"   || baseNameOf path != "result")) ./.;
+
+in
+  builtins.deepSeq
+    (builtins.readFile ./package.yaml)
+    (pkgs.haskellPackages.callCabal2nix "aoc" src {})
