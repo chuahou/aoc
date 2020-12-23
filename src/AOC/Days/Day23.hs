8b91cd0812d84c4816f757e5dfa7fde4a58c80a0
Chua Hou <human+github@chuahou.dev>
2020-12-23 18:15:11 +0800

    day23: redo part 1 with lists and 'Maybe's :(

--- src/AOC/Days/Day23.hs
+++ src/AOC/Days/Day23.hs
@@ -4,42 +4,36 @@
 module AOC.Days.Day23 (solution) where
 
 import           AOC.Solution
-
-import           Data.List.NonEmpty (NonEmpty (..))
+import           Control.Monad ((<=<))
+import           Data.List     (elemIndex)
 
 type Cup  = Int
-type Game = (Cup, Cup, Cup, Cup, NonEmpty Cup) -- at least 5 cups
+type Game = [Cup]
 
-rotate :: Game -> Game
-rotate (a, b, c, d, e:|f:fs) = (b, c, d, e, f :| fs <> [a])
-rotate (a, b, c, d, e:|[])   = (b, c, d, e, a :| [])
+splitElem :: Eq a => a -> [a] -> Maybe ([a], [a])
+splitElem x xs =   elemIndex x xs
+               >>= \idx -> case splitAt idx xs of
+                             (bef, _:aft) -> Just (bef, aft)
+                             _            -> Nothing
 
-stepGame :: Game -> Game
-stepGame (curr, p1, p2, p3, cups) = rotate $ tryDest (curr - 1)
+stepGame :: Int -> Game -> Maybe Game
+stepGame maxCup (curr:p1:p2:p3:rest) = go (curr - 1)
     where
-        tryDest x
-            | x < minimum' cups     = tryDest $ maximum' cups
-            | x `elem` [p1, p2, p3] = tryDest $ x - 1
-            | otherwise             = go x ([], cups)
-        go x (skipped, y:|ys)
-            | x == y = case skipped of
-                         []             -> (curr,  y, p1, p2, p3:|ys)
-                         [s1]           -> (curr, s1,  y, p1, p2:|p3:ys)
-                         [s1, s2]       -> (curr, s1, s2,  y, p1:|p2:p3:ys)
-                         [s1, s2, s3]   -> (curr, s1, s2, s3,  y:|p1:p2:p3:ys)
-                         s1:s2:s3:s4:ss -> (curr, s1, s2, s3, s4:|rest)
-                            where rest = ss <> (y:p1:p2:p3:ys)
-            | otherwise = case ys of
-                            (z:zs) -> go x (skipped <> [y], z:|zs)
-                            []     -> tryDest $ x - 1
+        go x
+          | x < 1                 = go maxCup
+          | x `elem` [p1, p2, p3] = go (x - 1)
+          | otherwise             =   splitElem x rest
+                                  >>= \(bef, aft) ->
+                                      Just $ bef <> (x:p1:p2:p3:aft) <> [curr]
+stepGame _ _ = Nothing
 
-solution :: Game :=> Int
+solution :: Game :=> Maybe Int
 solution = simpleSolution
-    (mkGame . map (read . pure) . filter (/= '\n'))
-    (showGame . (!! 100) . iterate stepGame)
-    undefined -- part2
-    where
-        mkGame (a:b:c:d:e:fs) = Just (a, b, c, d, e:|fs)
-        mkGame _              = Nothing
-        showGame (1, b, c, d, e:|fs) = read $ concatMap show (b:c:d:e:fs)
-        showGame g                   = showGame $ rotate g
+    (Just . map (read . pure) . filter (/= '\n'))
+    (showGame <=< (!? 100) . iterate' (stepGame 9))
+    undefined
+    where iterate' f x = case f x of
+                           Just x' -> x : iterate' f x'
+                           Nothing -> [x]
+          showGame g =   splitElem 1 g
+                     >>= \(bef, aft) -> readMaybe . concatMap show $ aft <> bef
