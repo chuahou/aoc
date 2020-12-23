50526d3257d40c8f4e54934cc2b8a2af92bef05a
Chua Hou <human+github@chuahou.dev>
2020-12-24 00:07:50 +0800

    day23: use mutable unboxed vectors

--- src/AOC/Days/Day23.hs
+++ src/AOC/Days/Day23.hs
@@ -5,31 +5,34 @@ module AOC.Days.Day23 (solution) where
 
 import           AOC.Solution
 
-import           Control.Monad ((<=<))
-import           Data.List     (sortOn)
-import qualified Data.Vector   as V
+import           Control.Monad               ((<=<))
+import           Control.Monad.ST            (ST, runST)
+import           Data.List                   (sortOn)
+import qualified Data.Vector.Unboxed         as V
+import qualified Data.Vector.Unboxed.Mutable as VM
 
 -- 0th element is current cup; i-th element is the cup following the i-th cup
 type Game = V.Vector Int
 
-stepGame :: Int -> Int -> Game -> Maybe Game
-stepGame _       0     g = Just g
-stepGame maxCups iters g = do
-    { curr     <- g V.!? 0
-    ; pick1    <- g V.!? curr
-    ; pick2    <- g V.!? pick1
-    ; pick3    <- g V.!? pick2
-    ; next     <- g V.!? pick3
-    ; let dest =  checkDest (curr - 1) [pick1, pick2, pick3]
-    ; dnext    <- g V.!? dest
-    ; let g'   = g V.// [ (curr,  next)
-                        , (pick3, dnext)
-                        , (dest,  pick1)
-                        , (0,     next)
-                        ]
-    ; stepGame maxCups (iters - 1) g'
-    }
+stepGame :: Int -> Int -> Game -> Game
+stepGame maxCups iters g = runST (V.thaw g >>= go iters >>= V.freeze)
     where
+        go :: Int -> VM.MVector s Int -> ST s (VM.MVector s Int)
+        go 0 v = pure v
+        go i v = do
+            { curr     <- v `VM.read` 0
+            ; pick1    <- v `VM.read` curr
+            ; pick2    <- v `VM.read` pick1
+            ; pick3    <- v `VM.read` pick2
+            ; next     <- v `VM.read` pick3
+            ; let dest =  checkDest (curr - 1) [pick1, pick2, pick3]
+            ; dnext    <- v `VM.read` dest
+            ; VM.write v curr  next
+            ; VM.write v pick3 dnext
+            ; VM.write v dest  pick1
+            ; VM.write v 0     next
+            ; go (i - 1) v
+            }
         checkDest x ps
             | x <= 0      = checkDest maxCups ps
             | x `elem` ps = checkDest (x - 1) ps
@@ -38,8 +41,8 @@ stepGame maxCups iters g = do
 solution :: [Int] :=> Maybe Int
 solution = simpleSolution
     (Just . map (read . pure) . filter (/= '\n'))
-    (showGame <=< stepGame 9       100      <=< mkGame)
-    (getStars <=< stepGame 1000000 10000000 <=< mkGame . (<> [10..1000000]))
+    (showGame . stepGame 9       100      <=< mkGame)
+    (getStars . stepGame 1000000 10000000 <=< mkGame . (<> [10..1000000]))
     where
         showGame g = go (g V.!? 1) >>= readMaybe . concatMap show
             where
