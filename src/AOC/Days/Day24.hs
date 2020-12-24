90817073b2ae71b331a1e466e1ba6d9ab26bb37d
Chua Hou <human+github@chuahou.dev>
2020-12-25 02:13:28 +0800

    day24: rewrite part 1 in list ops

--- src/AOC/Days/Day24.hs
+++ src/AOC/Days/Day24.hs
@@ -6,9 +6,11 @@ module AOC.Days.Day24 (solution) where
 import           AOC.Parsec
 import           AOC.Solution
 
-import           Data.Bifunctor (bimap)
-import           Data.Set       (Set)
-import qualified Data.Set       as Set
+import           Data.Bifunctor     (bimap)
+import           Data.List          (sort)
+import qualified Data.List.NonEmpty as NE
+import           Data.Set           (Set)
+import qualified Data.Set           as Set
 
 {-
     O - O - O   : (1, 2) (2, 2) (3, 2)
@@ -33,15 +35,11 @@ instrP =   bimap sum sum . unzip <$> many go <* endOfLine
            <|> (char 'e' >> pure ( 1, 0))
            <|> (char 'w' >> pure (-1, 0))
 
-part1 :: [Instr] -> Hex
-part1 = foldl' s Set.empty
-    where
-        s h i
-            | i `Set.member` h = i `Set.delete` h
-            | otherwise        = i `Set.insert` h
+part1 :: [Instr] -> [(Int, Int)]
+part1 = map NE.head . filter (odd . length) . NE.group . sort
 
 solution :: [Instr] :=> Int
 solution = simpleSolution
     (fromParsec $ many instrP)
-    (Set.size . part1)
+    (length . part1)
     undefined -- part2
