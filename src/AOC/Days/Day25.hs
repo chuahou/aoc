fd92ede49bb0ca0535fa0f85d32aeb198f05ef48
Chua Hou <human+github@chuahou.dev>
2020-12-25 13:51:43 +0800

    day25: add part 1
    - Oh my god it's DHKE

--- src/AOC/Days/Day25.hs
+++ src/AOC/Days/Day25.hs
@@ -3,10 +3,25 @@
 
 module AOC.Days.Day25 (solution) where
 
+import           AOC.Parsec
 import           AOC.Solution
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
-    undefined -- part2
+findSecret :: Int -> Int -> Int -> Int
+findSecret g p pk = fst . (!! 0) . dropWhile ((/= pk) . snd) . zip [0..]
+                  $ iterate ((`mod` p) . (* g)) 1
+
+findSymmetric :: Int -> Int -> Int -> Int
+findSymmetric p skA pkB = go skA pkB
+    where
+        go 1 x = x
+        go n x = go (n - 1) $ x * pkB `mod` p
+
+solution :: (Int, Int) :=> Int
+solution = simpleSolution
+    (fromParsec $ (,) <$> readP (many1 digit) <* endOfLine
+                      <*> readP (many1 digit))
+    (\(pkA, pkB) -> findSymmetric p (findSecret g p pkA) pkB)
+    undefined
+    where
+        g = 7        -- element
+        p = 20201227 -- prime
