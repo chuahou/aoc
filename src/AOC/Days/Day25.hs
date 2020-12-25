59f59eb98abb98d77b1d126dc336598f929158ca
Chua Hou <human+github@chuahou.dev>
2020-12-25 15:15:43 +0800

    day25: rewrite with 'arithmoi'

--- src/AOC/Days/Day25.hs
+++ src/AOC/Days/Day25.hs
@@ -1,27 +1,35 @@
 -- SPDX-License-Identifier: MIT
 -- Copyright (c) 2020 Chua Hou
+--
+-- I was unfamiliar with 'DataKinds', 'KnownNats' etc. so I have taken reference
+-- from https://github.com/glguy/advent2020/blob/master/execs/Day25.hs.
+
+{-# LANGUAGE DataKinds #-}
 
 module AOC.Days.Day25 (solution) where
 
 import           AOC.Parsec
 import           AOC.Solution
 
-findSecret :: Int -> Int -> Int -> Int
-findSecret g p pk = fst . (!! 0) . dropWhile ((/= pk) . snd) . zip [0..]
-                  $ iterate ((`mod` p) . (* g)) 1
+import           Math.NumberTheory.Moduli
+import           Math.NumberTheory.Moduli.Singleton
 
-findSymmetric :: Int -> Int -> Int -> Int
-findSymmetric p skA pkB = go skA pkB
-    where
-        go 1 x = x
-        go n x = go (n - 1) $ x * pkB `mod` p
+type P = 20201227
 
-solution :: (Int, Int) :=> Int
+dh :: Int -> Int -> Int -> Maybe Int
+dh g pkA pkB = do
+    { cg <- cyclicGroup :: Maybe (CyclicGroup Integer P)
+    ; rt <- isPrimitiveRoot cg (fromIntegral g :: Mod P)
+    ; x  <- isMultElement (fromIntegral pkA :: Mod P)
+    ; let skA = discreteLogarithm cg rt x
+    ; pure . fromInteger . getVal $ powMod (fromIntegral pkB :: Mod P) skA
+    }
+
+solution :: (Int, Int) :=> Maybe Int
 solution = simpleSolution
     (fromParsec $ (,) <$> readP (many1 digit) <* endOfLine
                       <*> readP (many1 digit))
-    (\(pkA, pkB) -> findSymmetric p (findSecret g p pkA) pkB)
-    fst -- there's no part 2
+    (uncurry $ dh g)
+    (const Nothing) -- there's no part 2
     where
         g = 7        -- element
-        p = 20201227 -- prime
