6b6b8bddb69cd1a822bc37374c9622e19d432aa8
Chua Hou <human+github@chuahou.dev>
2021-12-01 06:34:12 +0000

    day01: add part 2

--- src/AOC/Days/Day01.hs
+++ src/AOC/Days/Day01.hs
@@ -9,8 +9,12 @@ part1 :: [Int] -> Int
 part1 []     = 0
 part1 (x:xs) = length . filter (uncurry (<)) $ zip (x:xs) xs
 
+part2 :: [Int] -> Int
+part2 (x:y:zs) = part1 . map (\(a, b, c) -> a + b + c) $ zip3 (x:y:zs) (y:zs) zs
+part2 _        = 0
+
 solution :: [Int] :=> Int
 solution = simpleSolution
     (traverse readMaybe . lines)
     part1
-    undefined -- part2
+    part2
