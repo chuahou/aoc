9e6853a56d6cbff6a88da015d6883e087d645af4
Chua Hou <human+github@chuahou.dev>
2021-12-01 22:29:03 +0000

    day01: aw yeah

--- src/AOC/Days/Day01.hs
+++ src/AOC/Days/Day01.hs
@@ -4,17 +4,11 @@
 module AOC.Days.Day01 (solution) where
 
 import           AOC.Solution
+import           Control.Monad ((>=>))
 
-part1 :: [Int] -> Int
-part1 []     = 0
-part1 (x:xs) = length . filter (uncurry (<)) $ zip (x:xs) xs
-
-part2 :: [Int] -> Int
-part2 (x:y:z:zs) = length . filter (uncurry (<)) $ zip (x:y:z:zs) zs
-part2 _          = 0
+go :: Int -> [Int] -> Int
+go n xs = maybe 0 (length . filter (uncurry (<)) . zip xs) $
+    foldr (>=>) pure (replicate n tail) xs
 
 solution :: [Int] :=> Int
-solution = simpleSolution
-    (traverse readMaybe . lines)
-    part1
-    part2
+solution = simpleSolution (traverse readMaybe . lines) (go 1) (go 3)
