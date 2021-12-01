a3117993c24caa4b46191590487f40a53c23d2a3
Chua Hou <human+github@chuahou.dev>
2021-12-01 06:30:38 +0000

    day01: add part 1

--- src/AOC/Days/Day01.hs
+++ src/AOC/Days/Day01.hs
@@ -5,8 +5,12 @@ module AOC.Days.Day01 (solution) where
 
 import           AOC.Solution
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
+part1 :: [Int] -> Int
+part1 []     = 0
+part1 (x:xs) = length . filter (uncurry (<)) $ zip (x:xs) xs
+
+solution :: [Int] :=> Int
+solution = simpleSolution
+    (traverse readMaybe . lines)
+    part1
     undefined -- part2
