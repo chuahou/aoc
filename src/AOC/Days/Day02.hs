68c76a8b8d0f16be8929d3c2df894b8275c79b63
Chua Hou <human+github@chuahou.dev>
2021-12-02 08:08:58 +0000

    day02: add part 1

--- src/AOC/Days/Day02.hs
+++ src/AOC/Days/Day02.hs
@@ -3,10 +3,24 @@
 
 module AOC.Days.Day02 (solution) where
 
+import           AOC.Parsec
 import           AOC.Solution
+import           Data.Bifunctor (bimap)
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
+type Instruction = (Int, Int)
+
+instructionP :: Parser Instruction
+instructionP = do
+    dirn <- choice $ map try [ string "forward" >> pure (1,  0)
+                             , string "down"    >> pure (0,  1)
+                             , string "up"      >> pure (0, -1)
+                             ]
+    _    <- char ' '
+    num  <- readP $ many digit
+    pure $ bimap (* num) (* num) dirn
+
+solution :: [Instruction] :=> Int
+solution = simpleSolution
+    (fromParsec $ sepEndBy instructionP (char '\n'))
+    (uncurry (*) . foldl' (\(x1, y1) (x2, y2) -> (x1 + x2, y1 + y2)) (0, 0))
     undefined -- part2
