46a9d1079baac36781984f78420c9e96383436e3
Chua Hou <human+github@chuahou.dev>
2021-12-04 00:53:58 +0000

    day03: add part 1

--- src/AOC/Days/Day03.hs
+++ src/AOC/Days/Day03.hs
@@ -3,10 +3,32 @@
 
 module AOC.Days.Day03 (solution) where
 
+import           AOC.Parsec
 import           AOC.Solution
+import           Data.List    (transpose)
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
+data Bit = Zero | One deriving (Show, Eq)
+type Number = [Bit]
+
+bitP :: Parser Bit
+bitP = (char '1' >> pure One) <|> (char '0' >> pure Zero)
+
+part1 :: [Number] -> Int
+part1 xs =
+    let numOnes = map (length . filter (== One)) . transpose $ xs
+        numNums = length xs
+        gamma   = map (\n -> if n > numNums `div` 2 then One else Zero) numOnes
+        epsilon = map (\case One -> Zero; Zero -> One) gamma
+     in fromNumber gamma * fromNumber epsilon
+
+fromNumber :: Num a => Number -> a
+fromNumber = foldl' s 0
+    where
+        s n One  = s n Zero + 1
+        s n Zero = n * 2
+
+solution :: [Number] :=> Int
+solution = simpleSolution
+    (fromParsec $ endBy (many bitP) (char '\n'))
+    part1
     undefined -- part2
