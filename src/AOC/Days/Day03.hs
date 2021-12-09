445aaceee3924fd9025c89e7389bcff082db6b96
Chua Hou <human+github@chuahou.dev>
2021-12-09 22:40:28 +0800

    day03: add part 2
    - Been too long, jetlag/sleep deprivation is real

--- src/AOC/Days/Day03.hs
+++ src/AOC/Days/Day03.hs
@@ -21,14 +21,35 @@ part1 xs =
         epsilon = map (\case One -> Zero; Zero -> One) gamma
      in fromNumber gamma * fromNumber epsilon
 
+part2 :: [Number] -> Maybe Int
+part2 xs = (*) <$> (fromNumber <$> go False (zip xs xs))
+               <*> (fromNumber <$> go True  (zip xs xs))
+    where
+        go :: Bool -- True iff filter out most common.
+           -> [(Number, Number)] -> Maybe Number
+        go _ []  = Nothing
+        go _ [x] = pure $ fst x
+        go b xs' = (mapM tail' . filter ((b /=)
+                                       . (== Just (mostCommon $ map snd xs'))
+                                       . head . snd) $ xs') >>= go b
+            where
+                tail' (_,  [])   = Nothing
+                tail' (ys, _:zs) = Just (ys, zs)
+
+        mostCommon :: [Number] -> Bit
+        mostCommon xs' = if (length . filter (== Just One) . map head $ xs') * 2
+                                >= length xs'
+                            then One else Zero
+
+
 fromNumber :: Num a => Number -> a
 fromNumber = foldl' s 0
     where
         s n One  = s n Zero + 1
         s n Zero = n * 2
 
-solution :: [Number] :=> Int
+solution :: [Number] :=> Maybe Int
 solution = simpleSolution
     (fromParsec $ endBy (many bitP) (char '\n'))
-    part1
-    undefined -- part2
+    (pure . part1)
+    part2
