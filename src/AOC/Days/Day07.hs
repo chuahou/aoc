38b942ffd8169172a950671efacc93dc25448cd0
Chua Hou <human+github@chuahou.dev>
2021-12-10 23:03:55 +0800

    day07: add part 2

--- src/AOC/Days/Day07.hs
+++ src/AOC/Days/Day07.hs
@@ -6,6 +6,7 @@ module AOC.Days.Day07 (solution) where
 import           AOC.Parsec
 import           AOC.Solution
 
+import           Data.Array         (listArray, (!))
 import           Data.List          (sort)
 import           Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NE
@@ -15,14 +16,21 @@ type Pos = Int
 freqs :: [Pos] -> [(Pos, Int)]
 freqs = map (\(x:|xs) -> (x, NE.length $ x:|xs)) . NE.group . sort
 
-cost :: [(Pos, Int)] -> Pos -> Int
-cost fs p = sum $ map (\(p', n) -> abs (p - p') * n) fs
+cost1 :: Pos -> [(Pos, Int)] -> Pos -> Int
+cost1 _ fs p = sum $ map (\(p', n) -> abs (p - p') * n) fs
 
-part1 :: [Pos] -> Maybe Pos
-part1 ps = maximum ps >>= \maxPos -> minimum $ map (cost $ freqs ps) [0 .. maxPos]
+cost2 :: Pos -> [(Pos, Int)] -> Pos -> Int
+cost2 maxPos fs p = sum $ map (\(p', n) -> memo ! abs (p - p') * n) fs
+    where
+        memo = listArray (0, maxPos) (map triangle [0 .. maxPos])
+        triangle 0 = 0
+        triangle n = memo ! (n - 1) + n
+
+solve :: (Pos -> [(Pos, Int)] -> Pos -> Int) -> [Pos] -> Maybe Pos
+solve cost ps = maximum ps >>= \maxPos -> minimum $ map (cost maxPos $ freqs ps) [0 .. maxPos]
 
 solution :: [Pos] :=> Maybe Pos
 solution = simpleSolution
     (fromParsec $ sepBy1 (readP $ many1 digit) (char ','))
-    part1
-    undefined -- part2
+    (solve cost1)
+    (solve cost2)
