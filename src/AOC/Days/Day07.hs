9e4d6cb9b5a2e47a1ed9f9bc67d93c066d72e877
Chua Hou <human+github@chuahou.dev>
2021-12-10 22:56:34 +0800

    day07: add part 1

--- src/AOC/Days/Day07.hs
+++ src/AOC/Days/Day07.hs
@@ -3,10 +3,26 @@
 
 module AOC.Days.Day07 (solution) where
 
+import           AOC.Parsec
 import           AOC.Solution
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
+import           Data.List          (sort)
+import           Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NE
+
+type Pos = Int
+
+freqs :: [Pos] -> [(Pos, Int)]
+freqs = map (\(x:|xs) -> (x, NE.length $ x:|xs)) . NE.group . sort
+
+cost :: [(Pos, Int)] -> Pos -> Int
+cost fs p = sum $ map (\(p', n) -> abs (p - p') * n) fs
+
+part1 :: [Pos] -> Maybe Pos
+part1 ps = maximum ps >>= \maxPos -> minimum $ map (cost $ freqs ps) [0 .. maxPos]
+
+solution :: [Pos] :=> Maybe Pos
+solution = simpleSolution
+    (fromParsec $ sepBy1 (readP $ many1 digit) (char ','))
+    part1
     undefined -- part2
