557f8dd68a77215c15239e1ab6d89c477535176b
Chua Hou <human+github@chuahou.dev>
2021-12-10 21:10:47 +0800

    day06: add part 1

--- src/AOC/Days/Day06.hs
+++ src/AOC/Days/Day06.hs
@@ -3,10 +3,33 @@
 
 module AOC.Days.Day06 (solution) where
 
+import           AOC.Parsec
 import           AOC.Solution
+import           Data.List          (sort)
+import           Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NE
+import           Data.Maybe         (fromMaybe)
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
+type Counts = (Int, Int, Int, Int, Int, Int, Int, Int, Int)
+
+parser :: Parser Counts
+parser = do
+    fs <- sepBy1 (readP $ many1 digit) (char ',') :: Parser [Int]
+    let freqs = map (\(x:|xs) -> (x, NE.length $ x:|xs)) . NE.group . sort $ fs
+    let lookup' n = fromMaybe 0 $ lookup n freqs
+    pure (lookup' 0, lookup' 1, lookup' 2, lookup' 3, lookup' 4, lookup' 5,
+            lookup' 6, lookup' 7, lookup' 8)
+
+step :: Counts -> Counts
+step (n0, n1, n2, n3, n4, n5, n6, n7, n8) =
+    (n1, n2, n3, n4, n5, n6, n7 + n0, n8, n0)
+
+sumCounts :: Counts -> Int
+sumCounts (n0, n1, n2, n3, n4, n5, n6, n7, n8) =
+    sum [n0, n1, n2, n3, n4, n5, n6, n7, n8]
+
+solution :: Counts :=> Int
+solution = simpleSolution
+    (fromParsec parser)
+    (sumCounts . (!! 80) . iterate step)
     undefined -- part2
