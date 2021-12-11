68d21ffeb1bc75e4e0eb624217d056a6e82a2993
Chua Hou <human+github@chuahou.dev>
2021-12-11 09:14:07 +0800

    day08: add part 1

--- src/AOC/Days/Day08.hs
+++ src/AOC/Days/Day08.hs
@@ -3,10 +3,31 @@
 
 module AOC.Days.Day08 (solution) where
 
+import           AOC.Parsec
 import           AOC.Solution
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
+data Segment = A | B | C | D | E | F | G deriving (Show, Eq)
+type Pattern = [Segment]
+type Entry   = ([Pattern], [Pattern])
+
+entryP :: Parser Entry
+entryP = do
+    uniq <- count 10 (patternP <* char ' ') <* char '|'
+    outs <- count 4  (char ' ' *> patternP)
+    pure (uniq, outs)
+    where
+        patternP = many1 segmentP
+        segmentP = choice [ char 'a' >> pure A
+                          , char 'b' >> pure B
+                          , char 'c' >> pure C
+                          , char 'd' >> pure D
+                          , char 'e' >> pure E
+                          , char 'f' >> pure F
+                          , char 'g' >> pure G
+                          ]
+
+solution :: [Entry] :=> Int
+solution = simpleSolution
+    (fromParsec $ endBy entryP (char '\n'))
+    (length . filter ((`elem` [2, 3, 4, 7]) . length) . concatMap snd)
     undefined -- part2
