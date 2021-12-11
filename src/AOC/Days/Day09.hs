76599237e95a55566771115634ce9ee38b3f123a
Chua Hou <human+github@chuahou.dev>
2021-12-11 16:22:10 +0800

    day09: add part 1

--- src/AOC/Days/Day09.hs
+++ src/AOC/Days/Day09.hs
@@ -3,10 +3,37 @@
 
 module AOC.Days.Day09 (solution) where
 
-import           AOC.Solution
+import           Prelude         hiding ((!?))
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
+import           AOC.Parsec
+import           AOC.Solution
+import           Data.Map.Strict (Map, (!?))
+import qualified Data.Map.Strict as Map
+import           Data.Maybe      (mapMaybe)
+
+type Heightmap = Map Pos Int
+type Pos       = (Int, Int)
+
+heightmapP :: Parser Heightmap
+heightmapP = do
+    xss <- endBy1 (many (readP $ (:[]) <$> digit)) (char '\n')
+    let xss' = zipWith (\y hs -> zipWith (\x h -> ((x, y), h)) [0..] hs) [0..] xss
+    let xs'  = concat xss'
+    pure $ Map.fromList xs'
+
+isLowPoint :: Heightmap -> Pos -> Bool
+isLowPoint m (x, y) =
+    case m !? (x, y) of
+      Just height -> all (> height) . mapMaybe (m !?) $ [ (x - 1, y)
+                                                        , (x + 1, y)
+                                                        , (x,     y - 1)
+                                                        , (x,     y + 1)
+                                                        ]
+      Nothing     -> False
+
+solution :: Heightmap :=> Int
+solution = simpleSolution
+    (fromParsec heightmapP)
+    (sum . map ((+1) . snd) . Map.toList .
+        (\m -> Map.filterWithKey (flip . const $ isLowPoint m) m))
     undefined -- part2
