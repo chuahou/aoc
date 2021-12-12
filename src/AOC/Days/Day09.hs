a646aadb3f66c3a341d92a81875a52386516f297
Chua Hou <human+github@chuahou.dev>
2021-12-12 11:27:07 +0800

    day09: add part 2

--- src/AOC/Days/Day09.hs
+++ src/AOC/Days/Day09.hs
@@ -7,9 +7,13 @@ import           Prelude         hiding ((!?))
 
 import           AOC.Parsec
 import           AOC.Solution
+import           Data.Graph      (Graph, Vertex)
+import qualified Data.Graph      as Graph
+import           Data.List       (sort)
 import           Data.Map.Strict (Map, (!?))
 import qualified Data.Map.Strict as Map
-import           Data.Maybe      (mapMaybe)
+import           Data.Maybe      (isJust, mapMaybe)
+import qualified Data.Tree       as Tree
 
 type Heightmap = Map Pos Int
 type Pos       = (Int, Int)
@@ -24,16 +28,25 @@ heightmapP = do
 isLowPoint :: Heightmap -> Pos -> Bool
 isLowPoint m (x, y) =
     case m !? (x, y) of
-      Just height -> all (> height) . mapMaybe (m !?) $ [ (x - 1, y)
-                                                        , (x + 1, y)
-                                                        , (x,     y - 1)
-                                                        , (x,     y + 1)
-                                                        ]
+      Just height -> all (> height) . mapMaybe (m !?) $ adjacents (x, y)
       Nothing     -> False
+
+adjacents :: Pos -> [Pos]
+adjacents (x, y) = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+
+toGraph :: Heightmap -> (Graph, Vertex -> ((), Pos, [Pos]), Pos -> Maybe Vertex)
+toGraph m = Graph.graphFromEdges
+    [ ((), p, adjs)
+    | p <- filter ((/= Just 9) . (m !?)) $ Map.keys m
+    , let adjs = filter (\p' -> let mh = m !? p'
+                                 in isJust mh && mh /= Just 9)
+                        (adjacents p)
+    ]
 
 solution :: Heightmap :=> Int
 solution = simpleSolution
     (fromParsec heightmapP)
     (sum . map ((+1) . snd) . Map.toList .
         (\m -> Map.filterWithKey (flip . const $ isLowPoint m) m))
-    undefined -- part2
+    (product . take 3 . reverse . sort . map (length . Tree.flatten) .
+        Graph.components . (\(g, _, _) -> g) . toGraph)
