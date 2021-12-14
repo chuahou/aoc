1a5d6a30ef35b90e4168f598ad86fe2b9f57bf74
Chua Hou <human+github@chuahou.dev>
2021-12-14 22:09:39 +0800

    day10: add part 1

--- src/AOC/Days/Day10.hs
+++ src/AOC/Days/Day10.hs
@@ -4,9 +4,31 @@
 module AOC.Days.Day10 (solution) where
 
 import           AOC.Solution
+import           Data.Maybe   (mapMaybe)
 
-solution :: a :=> b
-solution = undefined -- simpleSolution
-    undefined -- parse
-    undefined -- part1
+illegal :: String -> Maybe Char
+illegal = go []
+    where
+        go _        []       = Nothing
+        go bs       ('(':cs) = go ('(':bs) cs
+        go bs       ('[':cs) = go ('[':bs) cs
+        go bs       ('{':cs) = go ('{':bs) cs
+        go bs       ('<':cs) = go ('<':bs) cs
+        go ('(':bs) (')':cs) = go bs cs
+        go ('[':bs) (']':cs) = go bs cs
+        go ('{':bs) ('}':cs) = go bs cs
+        go ('<':bs) ('>':cs) = go bs cs
+        go _        (c:_)    = Just c
+
+badness :: Char -> Int
+badness ')' = 3
+badness ']' = 57
+badness '}' = 1197
+badness '>' = 25137
+badness _   = 0
+
+solution :: [String] :=> Int
+solution = simpleSolution
+    (pure . lines)
+    (sum . map badness . mapMaybe illegal)
     undefined -- part2
